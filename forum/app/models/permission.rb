bb9a85d85e19ebcb48c6799f525dccdd6af320fc
David Silver <dsilver@candidatemetrics.com>
2013-01-18 08:58:15 -0800

    spec admin permissions

--- forum/app/models/permission.rb
+++ forum/app/models/permission.rb
@@ -1,5 +1,9 @@
 class Permission < Struct.new(:user)
   def allow?(controller, action)
+    if user.nil?
     controller == "topics" && action.in?(%w[index show])
+    else user.admin?
+      true
+    end
   end
 end
