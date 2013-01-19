727d4b641901d5bcd7cb274190f8db99b8c0e250
David Silver <dsilver@candidatemetrics.com>
2013-01-19 08:36:11 -0800

    spec member

--- forum/app/models/permission.rb
+++ forum/app/models/permission.rb
@@ -1,9 +1,11 @@
 class Permission < Struct.new(:user)
   def allow?(controller, action)
     if user.nil?
-    controller == "topics" && action.in?(%w[index show])
-    else user.admin?
+      controller == "topics" && action.in?(%w[index show])
+    elsif user.admin?
       true
+    else
+      controller == "topics" && action != 'destroy'
     end
   end
 end
